02aaea4c6186dcba1210b6f452bbdaa1e17210ee
Oliver Hulland <olihulland@gmail.com>
2023-05-10 00:08:27 +0100

    websocket using socketio to update data when new data added

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,12 @@ import {
   theme,
 } from "@chakra-ui/react";
 import { Header } from "./components/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Intro } from "./pages/Intro";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Data } from "./pages/Data";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { io } from "socket.io-client";
 
 export interface PageProps {
   setCurrentPhase: (phase: string | undefined) => void;
@@ -21,8 +22,37 @@ export const App = () => {
   const [currentPhase, setCurrentPhase] = useState<string | undefined>(
     undefined
   );
+  const [socketInstance, setSocketInstance] = useState<any>();
 
   const queryClient = new QueryClient();
+
+  useEffect(() => {
+    const socket = io(`${process.env.REACT_APP_API_WS}`, {
+      transports: ["websocket"],
+    });
+
+    setSocketInstance(socket);
+
+    socket.on("connect", () => {
+      console.log("ws: connect websocket");
+    });
+
+    socket.on("disconnect", (data) => {
+      console.log("ws: disconnect ws", data);
+    });
+
+    socket.on("data_update", (dataStr) => {
+      const data = JSON.parse(dataStr);
+      console.log("ws: data update");
+      if (data.queries_updated) {
+        queryClient.invalidateQueries({ queryKey: data.queries_updated });
+      }
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
