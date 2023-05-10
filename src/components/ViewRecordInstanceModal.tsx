5c6d0fd537a5aa3905697f1405604d9f883c8048
Oliver Hulland <olihulland@gmail.com>
2023-05-10 14:12:45 +0100

    view all raw data

---
+++ src/components/ViewRecordInstanceModal.tsx
@@ -0,0 +1,96 @@
+import { RecordInstance } from "../data/types";
+import {
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  ModalBody,
+  Flex,
+  Heading,
+  Text,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from "@chakra-ui/react";
+
+interface Props {
+  viewRecordDisclosure: any;
+  openRecordInstance: RecordInstance | undefined;
+}
+
+export const ViewRecordInstanceModal = ({
+  viewRecordDisclosure,
+  openRecordInstance,
+}: Props) => {
+  const getKeys = (): string[] => {
+    let keys: string[] = [];
+    openRecordInstance?.data.forEach((dataPoint) => {
+      Object.keys(dataPoint).forEach((dpKey) => {
+        if (!keys.includes(dpKey)) keys.push(dpKey);
+      });
+    });
+    console.log("keys", keys);
+    return keys;
+  };
+
+  return (
+    <Modal
+      isOpen={viewRecordDisclosure.isOpen}
+      onClose={viewRecordDisclosure.onClose}
+      scrollBehavior={"inside"}
+      size={"6xl"}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>View Recording</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Flex gap={5}>
+            <Text>
+              <b>Record ID:</b> {openRecordInstance?.uniqueID}
+            </Text>
+            <Text>
+              <b>Micro:bit ID:</b> {openRecordInstance?.deviceID}
+            </Text>
+          </Flex>
+
+          <Text>
+            <b>Class:</b> {openRecordInstance?.classification}
+          </Text>
+
+          <Text>
+            <b>Number of Data Points: </b> {openRecordInstance?.data.length}
+          </Text>
+
+          <TableContainer>
+            <Table>
+              <Thead>
+                <Tr>
+                  {getKeys().map((k) => {
+                    return <Th>{k}</Th>;
+                  })}
+                </Tr>
+              </Thead>
+              <Tbody>
+                {openRecordInstance?.data.map((dataPoint) => {
+                  return (
+                    <Tr>
+                      {Object.values(dataPoint).map((d) => {
+                        return <Td>{d}</Td>;
+                      })}
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -15,16 +15,22 @@ import {
   Th,
   Td,
   Button,
+  useDisclosure,
 } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { PageProps } from "../App";
 import { useQuery } from "react-query";
 import { getData } from "../data/api";
 import { APIData, RecordInstance } from "../data/types";
+import { ViewRecordInstanceModal } from "../components/ViewRecordInstanceModal";
 
 export const Data = ({ setCurrentPhase }: PageProps) => {
   const getDataQuery: { data: APIData | undefined; [key: string]: any } =
     useQuery("getData", getData);
+
+  const viewRecordDisclosure = useDisclosure();
+  const [openRecordInstance, setOpenRecordInstance] =
+    useState<RecordInstance>();
 
   useEffect(() => {
     setCurrentPhase("Data Collection");
@@ -35,56 +41,68 @@ export const Data = ({ setCurrentPhase }: PageProps) => {
   }, [getDataQuery.data]);
 
   return (
-    <Container maxWidth="container.xl" px={10}>
-      <Heading>Raw Data</Heading>
-      {getDataQuery.isLoading && (
-        <VStack>
-          <Spinner size={"xl"} />
-          <Heading size={"md"}>Loading Data ...</Heading>
-        </VStack>
-      )}
-      {getDataQuery.isError && (
-        <Container>
-          <Alert status="error">
-            <AlertIcon />
-            <AlertTitle>Error Loading Data</AlertTitle>
-          </Alert>
-        </Container>
-      )}
-      {getDataQuery.isSuccess && (
-        <TableContainer>
-          <Table>
-            <Thead>
-              <Tr>
-                <Th>MicroBit ID</Th>
-                <Th>Record ID</Th>
-                <Th>Class</Th>
-                <Th></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {getDataQuery.data?.record_instances.map(
-                (record: RecordInstance) => (
-                  <Tr key={record.uniqueID}>
-                    <Td>{record.deviceID}</Td>
-                    <Td>{record.uniqueID}</Td>
-                    <Td>{record.classification}</Td>
-                    <Td>
-                      <Button
-                        onClick={() => {
-                          console.log(record);
-                        }}
-                      >
-                        View
-                      </Button>
-                    </Td>
-                  </Tr>
-                )
-              )}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      )}
-    </Container>
+    <>
+      <Container maxWidth="container.xl" px={10}>
+        <Heading>Raw Data</Heading>
+        {getDataQuery.isLoading && (
+          <VStack>
+            <Spinner size={"xl"} />
+            <Heading size={"md"}>Loading Data ...</Heading>
+          </VStack>
+        )}
+        {getDataQuery.isError && (
+          <Container>
+            <Alert status="error">
+              <AlertIcon />
+              <AlertTitle>Error Loading Data</AlertTitle>
+            </Alert>
+          </Container>
+        )}
+        {getDataQuery.isSuccess && (
+          <TableContainer>
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>MicroBit ID</Th>
+                  <Th>Record ID</Th>
+                  <Th>Class</Th>
+                  <Th></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {getDataQuery.data?.record_instances.map(
+                  (record: RecordInstance) => (
+                    <Tr key={record.uniqueID}>
+                      <Td>{record.deviceID}</Td>
+                      <Td>{record.uniqueID}</Td>
+                      <Td>
+                        {record.classification != null
+                          ? record.classification
+                          : "Not Set"}
+                      </Td>
+                      <Td>
+                        <Button
+                          onClick={() => {
+                            console.log(record);
+                            setOpenRecordInstance(record);
+                            viewRecordDisclosure.onOpen();
+                          }}
+                        >
+                          View
+                        </Button>
+                      </Td>
+                    </Tr>
+                  )
+                )}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        )}
+      </Container>
+      <ViewRecordInstanceModal
+        viewRecordDisclosure={viewRecordDisclosure}
+        openRecordInstance={openRecordInstance}
+      />
+    </>
   );
 };
